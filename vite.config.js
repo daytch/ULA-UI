5106f358afc435c80ef15272ba137c5667d6feaf
daytch <van.daytch@gmail.com>
2022-12-25 14:51:19 +0700

    fixing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import {viteCommonjs, esbuildCommonjs} from '@originjs/vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [viteCommonjs(),react()],
   server: {
     port: 3000,
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(['react-s3'])],
+    },
+  }
   build: {
     rollupOptions: {
       output: {
