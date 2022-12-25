db0e3918cc5406092abe203c913e2ae71aa18ed9
daytch <van.daytch@gmail.com>
2022-12-25 19:02:28 +0700

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginRequire from "vite-plugin-require";
+import requireTransform from "vite-plugin-require-transform";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
+  plugins: [react(), vitePluginRequire({}), requireTransform({})],
   server: {
     port: 3000,
   },
