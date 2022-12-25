c75406ee42657d09aa92155645b41fa0c54e1fca
daytch <van.daytch@gmail.com>
2022-12-25 15:07:48 +0700

    add vite-plugin-require

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import {viteCommonjs, esbuildCommonjs} from '@originjs/vite-plugin-commonjs'
+import vitePluginRequire from "vite-plugin-require";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteCommonjs(),react()],
+  plugins: [react(), vitePluginRequire()],
   server: {
     port: 3000,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(['react-s3'])],
-    },
   },
   build: {
     rollupOptions: {
