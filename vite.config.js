cfbbb98a4388ab93c66a811356c5e39dfce95e2f
daytch <van.daytch@gmail.com>
2022-12-25 21:18:10 +0700

    fixing

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import requireTransform from "vite-plugin-require-transform";
+import { viteCommonjs, esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), requireTransform({})],
+  plugins: [viteCommonjs(), react()],
   server: {
     port: 3000,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        // Solves:
+        // https://github.com/vitejs/vite/issues/5308
+        // add the name of your package
+        esbuildCommonjs(['dayjs', 'react-tailwindcss-datepicker']),
+      ],
+    },
   },
   build: {
     rollupOptions: {
